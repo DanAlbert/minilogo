d523e2ed0434feda63caf120d8de563005758f1e
Dan Albert <dan@gingerhq.net>
2013-04-29 19:53:44 -0700

    Updated for homework 2

--- minilogo.hs
+++ minilogo.hs
@@ -1,59 +1,28 @@
 module MiniLogo where
 
--- 1a
+import SVG (ppLines)
+
 data Mode = Up | Down
             deriving Show
 
-data Pos  = Num Int | Name String
-            deriving Show
-
-data Pars = PList String Pars | Param String
-            deriving Show
-
-data Vals = VList Int Vals | Val Int
-            deriving Show
-
 data Cmd = Pen Mode
-	     | MoveTo Pos Pos
-	     | Def String Pars Cmd
-	     | Call String Vals
-	     | CmdList Cmd Cmd
-         | Nop
+	     | MoveTo Int Int
+	     | Seq Cmd Cmd
            deriving Show
 
--- 1b
--- def vector (x0, y0, x1, y1) pen up; moveto x0 y0; pen down; moveto x1 y1;
+type State = (Mode, Int, Int)
 
-vector = Def "vector" (PList "x0" (PList "y0" (PList "x1" (Param "y1"))))
-         (CmdList
-           (Pen Up)
-           (CmdList
-             (MoveTo (Name "x0") (Name "y0"))
-             (CmdList
-               (Pen Down)
-                 (MoveTo (Name "x1") (Name "y1")))))
+type Line = (Int, Int, Int, Int)
+type Lines = [Line]
 
--- 1c
-steps :: Int -> Cmd
-steps 0 = Nop
-steps 1 = (CmdList
-            (Pen Up)
-            (CmdList
-              (MoveTo (Num 1) (Num 1))
-              (CmdList
-                (Pen Down)
-                (CmdList
-                  (MoveTo (Num 0) (Num 1))
-                  (MoveTo (Num 0) (Num 0))))))
+semS :: Cmd -> State -> (State, Lines)
+semS (Pen Up)        (_, x, y)    = ((Up, x, y), [])
+semS (Pen Down)      (_, x, y)    = ((Down, x, y), [])
+semS (MoveTo x y)    (Up, _, _)   = ((Up, x, y), [])
+semS (MoveTo x' y')  (Down, x, y) = ((Down, x', y'), [(x, y, x', y')])
+semS (Seq cmd0 cmd1) s0           = (s1, l0 ++ l1)
+                                    where (s2, l1) = semS cmd1 s1
+                                          (s1, l0) = semS cmd0 s0
 
-steps n = (CmdList
-            (Pen Up)
-            (CmdList
-              (MoveTo (Num n) (Num n))
-              (CmdList
-                (Pen Down)
-                (CmdList
-                  (MoveTo (Num (n - 1)) (Num n))
-                  (CmdList
-                    (MoveTo (Num (n - 1)) (Num (n - 1)))
-                    (steps (n - 1)))))))
+sem' :: Cmd -> Lines
+sem' cmd = snd (semS cmd (Up, 0, 0))
